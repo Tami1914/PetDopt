c3eed5f00781fabb3cbe61c8e95926f76bb0e199
Tami1914 <aythami.1914@gmail.com>
2024-04-11 19:07:47 +0100

    footer v2

--- PetDopt/src/assets/components/footer/Footer.jsx
+++ PetDopt/src/assets/components/footer/Footer.jsx
@@ -3,16 +3,19 @@ import "./Footer.css"
 function Footer() {
   return (
     <>
+
+      <div className="footer-desc">
+        <p></p>
+      </div>
+
       <div className="footer-term">
-        <p>2023-2024©Una plataforma de <a href="/home">PetDopt</a> con la colaboración
-          de Fundación Affinity
-        </p>
+        <p>2023-2024©Una plataforma de <a className="pet" href="/home">PetDopt</a> con la colaboración de Fundación Affinity</p>
         <br />
-        <a href="">Términos y condiciones de uso</a>
-        <a href="">Política y privacidad</a>
-        <a href="">Coockies</a>
-        <a href="">FAQ</a>
-        <a href="">Contaacto</a>
+        <a className="b" href="">Términos y condiciones de uso</a>
+        <a className="b" href="">Política y privacidad</a>
+        <a className="b" href="">Coockies</a>
+        <a className="b" href="">FAQ</a>
+        <a className="b" href="">Contacto</a>
       </div>
     </>
   )
